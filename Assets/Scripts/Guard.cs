d91e6d3947fe1963a0a57c08c28da72139c2aa89
VacantArkade <kadehuskey@gmail.com>
2025-09-15 17:28:12 -0500

    Save

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -4,16 +4,44 @@ using UnityEngine.AI;
 public class Guard : MonoBehaviour
 {
     [SerializeField] NavMeshAgent agent;
-    [SerializeField] Transform target;
+    NavMeshPath path;
+    //[SerializeField] Rigidbody rb;
+
+    [SerializeField] float speed;
+
+    [SerializeField] Transform[] patrolPoints;
+    Transform currentPatrolPoint;
+    int patrolPointIndex = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        agent.SetDestination(target.position);
+        currentPatrolPoint = patrolPoints[0];
+        agent.SetDestination(currentPatrolPoint.position);
+        //rb = GetComponent<Rigidbody>();
+        path = new NavMeshPath();
+
+        agent.CalculatePath(currentPatrolPoint.position, path);
     }
 
     // Update is called once per frame
     void Update()
+    {
+        float distance = Vector3.Distance(transform.position, currentPatrolPoint.position);
+
+
+        if(distance < 1)
+        {
+            patrolPointIndex++;
+
+            patrolPointIndex %= patrolPoints.Length;
+
+            currentPatrolPoint = patrolPoints[patrolPointIndex];
+            agent.SetDestination(currentPatrolPoint.position);
+        }
+    }
+
+    private void FixedUpdate()
     {
         
     }
