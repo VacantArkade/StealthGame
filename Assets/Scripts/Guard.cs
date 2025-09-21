6b1e144658ca4f686afb2bd68526abc3e66aa65d
VacantArkade <kadehuskey@gmail.com>
2025-09-20 22:09:35 -0500

    I see player now

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -6,12 +6,16 @@ public class Guard : MonoBehaviour
     [SerializeField] NavMeshAgent agent;
     NavMeshPath path;
     //[SerializeField] Rigidbody rb;
+    [SerializeField] GameObject player;
 
     [SerializeField] float speed;
 
     [SerializeField] Transform[] patrolPoints;
     Transform currentPatrolPoint;
     int patrolPointIndex = 0;
+
+    [SerializeField] float visionRadius;
+    [SerializeField] LayerMask environmentLayer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -39,10 +43,51 @@ public class Guard : MonoBehaviour
             currentPatrolPoint = patrolPoints[patrolPointIndex];
             agent.SetDestination(currentPatrolPoint.position);
         }
+
+        Debug.DrawLine(transform.position, transform.position + (transform.forward * 5), Color.red);
     }
 
     private void FixedUpdate()
     {
         
+    }
+
+    private void HeardSomething(Collider thingWeHeard)
+    {
+        if (thingWeHeard.GetComponent<Player>() != null)
+        {
+            Debug.Log("Did you hear that?");
+            Vector3 guardForward = transform.forward;
+        }
+    }
+
+    public void SawSomething(Collider thingWeSaw)
+    {
+        if(thingWeSaw.GetComponent<Player>() != null)
+        {
+            Vector3 guardForward = transform.forward;
+            guardForward.y = 0;
+            guardForward.Normalize();
+            Vector3 lineToPlayer = (thingWeSaw.transform.position - transform.position).normalized;
+            lineToPlayer.y = 0;
+            lineToPlayer.Normalize();
+            float dot = Vector3.Dot(guardForward, lineToPlayer);
+
+            if(dot > visionRadius)
+            {
+                RaycastHit hit;
+                if(Physics.Raycast(transform.position, lineToPlayer, out hit, 1000, environmentLayer))
+                {
+                    Debug.Log("Saw a wall");
+                }
+                else
+                    Debug.Log("Saw player");
+            }
+
+            else
+            {
+                Debug.Log("Did not see player");
+            }
+        }
     }
 }
